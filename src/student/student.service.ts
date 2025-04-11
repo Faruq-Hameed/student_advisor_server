b039bfc59db7e7975881b330c953dbd2d1ae83b9
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-04-11 17:52:08 +0100

    added student service

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { Student,StudentDocument } from './schemas/student.schema/student.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+    constructor(
+        @InjectModel(Student.name) private studentModel: Model<StudentDocument>,
+    ) {}
+    
+    async create(createStudentDto: CreateStudentDto): Promise<Student> {
+        const createdStudent = new this.studentModel(createStudentDto);
+        return createdStudent.save();
+    }
+    
+    async findAll(): Promise<Student[]> {
+        return this.studentModel.find().exec();
+    }
+    
+    async findOne(id: string): Promise<Student> {
+        return this.studentModel.findById(id).exec();
+    }
+    
+    async update(id: string, updateStudentDto: UpdateStudentDto): Promise<Student> {
+        return this.studentModel.findByIdAndUpdate(id, updateStudentDto, { new: true }).exec();
+    }
+    
+    async remove(id: string): Promise<Student> {
+        return this.studentModel.findByIdAndRemove(id).exec();
+    }
+}
